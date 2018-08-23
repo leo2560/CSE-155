8b457504fd4a252255c113c9f1d341c782c2d754
Maricel4 <42561008+Maricel4@users.noreply.github.com>
2018-08-23 15:55:51 -0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 /**
- * Author: Chris Bourke
- * Date: 2016/11/02
+ * Author: Claire and Maricel
+ * Date: 2018/23/08
  *
  * A simple hello world program in C
  *
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
 
-  printf("Claire - Meteorology\n");
+  printf("Claire - Meteorology, Maricel - Computer Science/Film\n");
 
   return 0;
 }
